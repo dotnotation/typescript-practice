f13536204c1a60c19bff00b48eef90c1790aade5
dotnotation <dotinmotionproductions@gmail.com>
2022-06-02 12:57:34 -0400

    temporarily commented out isolatedModules in tsconfig and playing with get elements from page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { v4 as uuidV4 } from "uuid"
+// import { v4 as uuidV4 } from "uuid"
 
-console.log(uuidV4())
+// console.log(uuidV4())
+
+const list = document.querySelector("#list")
+const form = document.querySelector("#new-task-form")
+const input = document.querySelector("#new-task-title")
