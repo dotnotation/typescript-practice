5a0948fead008cc2c482c8e05dae73e3045002a3
dotnotation <dotinmotionproductions@gmail.com>
2022-06-03 13:40:38 -0400

    saving tasks to an array and adding event listener to checkbox to update task completed status

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ type Task = {
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = []
 
 form?.addEventListener("submit", e => {
   e.preventDefault()
@@ -22,8 +23,10 @@ form?.addEventListener("submit", e => {
     completed: false,
     createdAt: new Date()
   }
-  
+  tasks.push(newTask)
+
   addListItem(newTask)
+
   input.value = ""
 })
 
@@ -31,6 +34,10 @@ function addListItem(task: Task){
   const item = document.createElement("li")
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    console.log(tasks)
+  })
   checkbox.type = "checkbox"
   checkbox.checked = task.completed
   label.append(checkbox, task.title)
